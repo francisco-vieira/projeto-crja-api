c4468802449e70df8294fa9e92993e7befedf677
francisco-vieira <francisco.vieira.dev@gmail.com>
2023-10-06 10:10:37 -0300

    Projeto inicializado

---
+++ src/main/java/br/crja/com/projetocrjaapi/CrjaApiApplication.java
@@ -0,0 +1,13 @@
+package br.crja.com.projetocrjaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrjaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrjaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/crja/com/projetocrjaapi/CrjaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.crja.com.projetocrjaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrjaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
