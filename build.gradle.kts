fc09e54c888751276be3f36cd330d39316627f65
francisco-vieira <francisco.vieira.dev@gmail.com>
2023-10-06 22:59:50 -0300

    Adicionando main-class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,42 @@
 plugins {
-	java
-	id("org.springframework.boot") version "2.7.16"
-	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    java
+    id("org.springframework.boot") version "2.7.16"
+    id("io.spring.dependency-management") version "1.0.15.RELEASE"
 }
 
 group = "br.crja.com"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_1_8
-	manifest {
-		attributes(
-			mutableMapOf("Main-Class" to "br.crja.com.projeto-crja.CrjaApiApplication")
-		)
-	}
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    manifest {
+        attributes(
+            mutableMapOf("Main-Class" to "br.crja.com.projeto-crja.CrjaApiApplication")
+        )
+    }
+    tasks.jar {
+        archiveClassifier.set("")
+    }
+    tasks.bootJar {
+        archiveClassifier.set("boot")
+    }
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.postgresql:postgresql")
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.postgresql:postgresql")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
